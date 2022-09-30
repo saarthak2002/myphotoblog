3c309fff82bcb58b9e83573168a361aa69756271
Saarthak Gupta <saarthakvir@gmail.com>
2022-09-30 16:53:52 -0400

    added photogrid to san francisco on button click

--- src/SanFrancisco.js
+++ src/SanFrancisco.js
@@ -1,11 +1,26 @@
 import { NavBar } from "./Navbar";
 import { Footer } from './Footer'
-
+import './ImageGrid.css';
 export function SanFrancisco(){
     return(
         <div className="aboutPage">
             <NavBar />
-            <h1>SF</h1>
+            <h1 className="SFHeading">San Francisco</h1>
+            <div className="row">
+                <div className="column">
+                    <img src={require("./Images/ghirardellisquare.png")} alt={"Ghiradelli Square in SF"}/>  
+                    <img src={require("./goldengatebridge.png")} alt={"Golden Gate Bridge in SF"}/>
+                </div>
+                <div className="column">               
+                    <img src={require("./washmonu.png")} alt={"Ghiradelli Square in SF"}/> 
+                </div>
+                <div className="column">
+                    <img src={require("./nyc.png")} alt={"Ghiradelli Square in SF"}/> 
+                </div>
+                <div className="column">
+                    <img src={require("./washmonu.png")} alt={"Ghiradelli Square in SF"}/> 
+                </div>
+            </div>  
             <Footer />
         </div>
     );
