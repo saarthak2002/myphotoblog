3f25a8b119eb84b7d3f105dc76aa729efe74cb18
Saarthak Gupta <saarthakvir@gmail.com>
2022-09-30 21:06:04 -0400

    setup san francisco photo grid

--- src/SanFrancisco.js
+++ src/SanFrancisco.js
@@ -8,17 +8,25 @@ export function SanFrancisco(){
             <h1 className="SFHeading">San Francisco</h1>
             <div className="row">
                 <div className="column">
-                    <img src={require("./Images/ghirardellisquare.png")} alt={"Ghiradelli Square in SF"}/>  
-                    <img src={require("./goldengatebridge.png")} alt={"Golden Gate Bridge in SF"}/>
+                    <img src={require("./Images/SF/alcatrazisland.png")} alt={"Alcatraz Island"}/>  
+                    <img src={require("./Images/SF/californiamural.png")} alt={"A mural of the word California"}/>
+                    <img src={require("./Images/SF/lombardstreet1.png")} alt={"Lombard Street in San Francisco"}/>
+                    <img src={require("./Images/SF/torpedopoint.png")} alt={"A pier in the ocean with San Francisco in the background"}/>
                 </div>
                 <div className="column">               
-                    <img src={require("./washmonu.png")} alt={"Ghiradelli Square in SF"}/> 
+                    <img src={require("./Images/SF/beachships.png")} alt={"Large ships near a beach"}/>
+                    <img src={require("./Images/SF/ghirardellisquare.png")} alt={"Famous Ghirardelli Square in San Francisco"}/>
+                    <img src={require("./Images/SF/pier39.png")} alt={"Pier 39 in San Francisco"}/>
                 </div>
                 <div className="column">
-                    <img src={require("./nyc.png")} alt={"Ghiradelli Square in SF"}/> 
+                    <img src={require("./Images/SF/fishermanswharf.png")} alt={"Fisherman's Wharf in San Francisco"}/>
+                    <img src={require("./Images/SF/blueredbuilding.png")} alt={"Red and blue builings in SF"}/>
+                    <img src={require("./Images/SF/sffromsea.png")} alt={"View of San Francisco from the sea"}/>
                 </div>
                 <div className="column">
-                    <img src={require("./washmonu.png")} alt={"Ghiradelli Square in SF"}/> 
+                    <img src={require("./Images/SF/boatsinmarina.png")} alt={"Boats moored in a marina"}/>
+                    <img src={require("./Images/SF/goldengateoverlook.png")} alt={"View overlooking the Golden Gate Bridge"}/>
+                    <img src={require("./Images/SF/lombardstreetoverlook.png")} alt={"Sceneic view overlooking Lombard Street and San Francisco"}/>
                 </div>
             </div>  
             <Footer />
