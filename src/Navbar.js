3eb6319deb21b7ad387b28153080e60fc0bb6410
Saarthak Gupta <saarthakvir@gmail.com>
2022-10-02 17:39:27 -0400

    added clicl title to return home

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,18 @@
 import {NavLink} from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 export function NavBar() {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    console.log('clicked');
+    let path=`/`;
+    navigate(path);
+  }
     return(
     <div>
-        <h1 className='title'>Saarthak Gupta</h1>
+      <div className='titleContainer'>
+        <h1 className='title' onClick={routeChange}>Saarthak Gupta</h1>
+      </div>
+        
       <nav className="navbarmain">
         
         <NavLink to="/" className="nav_link">Home</NavLink>
